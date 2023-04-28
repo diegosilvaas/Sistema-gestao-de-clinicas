75e498cd0b8bca8248f31a713008e8945237a1d0
diegosilvaas <diegomecanica2014@gmail.com>
2023-04-28 17:14:21 -0400

    criando classe

---
+++ app/Model/Cliente.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once 'Conection.php';
+
+class Cliente {
+
+    public $nome;
+    public $data_nascimento;
+    public $telefone;
+    public $queixa_principal;
+    public $realizou_cirurgia;
+    public $toma_remedio;
+    public $toma_anticoncepcional;
+    public $tratamento_medico;
+    public $hipo_arterial;
+    public $cardiaco;
+    public $disturbio_hormonal;
+    public $disturbio_circulatorio;
+    public $doenca_pele;
+    public $ingere_alcool;
+    public $diabetes;
+    public $quadro_saude;
+    public $gestante;
+    public $atividade_fisica;
+    public $alimentacao_balanceada;
+    public $ingestao_agua;
+    public $funcionamento_intestinal;
+    public $ciclo_mestrual;
+    public $possui_marcapasso;
+    public $possui_varizes;
+    public $cosmetico_corporal;
+    public $epilepsia;
+    public $cicatriz;
+    public $engravidou;
+    public $protese_metalica;
+    public $fumante;
+    public $cancer;
+    
+
+
+    public function cadastrarCliente()
+    {
+        
+    }
+
+
+
+
+
+}
+
