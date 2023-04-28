bfc5b07f98ea0329b696653e9284239f91ad0ec5
diegosilvaas <diegomecanica2014@gmail.com>
2023-04-28 17:14:45 -0400

    conexao do banco

---
+++ app/Model/Conection.php
@@ -0,0 +1,33 @@
+<?php
+
+class Conection {
+
+    protected static $db;
+    private $host = 'localhost';
+    private $password = '';
+    private $user = 'root';
+    private $dbName = 'clinica_estetica';
+
+    public function __construct()
+    {
+     $this->connect();
+    }
+
+    public function connect()
+    {
+        try {
+            self::$db = new PDO("mysql:dbname=$this->dbName;host=$this->host", $this->user, $this->password);
+
+        } catch (PDOException $erro){
+            echo $erro->getMessage();
+        }
+    }
+
+    public static function getDb ()
+    {
+        return self::$db;
+
+    }
+
+
+}
