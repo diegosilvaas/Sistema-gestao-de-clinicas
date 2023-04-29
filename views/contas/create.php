b3847d921706b8c120d89d20cae811643bc27174
diegosilvaas <diegomecanica2014@gmail.com>
2023-04-29 16:27:55 -0400

    atualizando contas a receber

--- views/contas/create.php
+++ views/contas/create.php
@@ -27,56 +27,85 @@
             <!-- Navbar -->
 
             <?php include_once '../../includes/navbar.php'; ?>
+            <div class="layout-container">
+            <div class="col-12">
+                  <div class="card mb-4">
+                    <div class="card-header d-flex align-items-center justify-content-between">
+                      <h5 class="mb-0">Cadastro de receber</h5>
+                      <small class="text-muted float-end">Dados de cadastro</small>
+                    </div>
+                    <div class="card-body">
+                      <form>
+                        <div class="row mb-3">
+                          <label class="col-sm-2 col-form-label" for="basic-icon-default-fullname">Cliente</label>
+                          <div class="col-sm-10">
+                            <div class="input-group input-group-merge">
+                              <span id="basic-icon-default-fullname2" class="input-group-text"><i class="bx bx-user"></i></span>
+                              <input type="text" class="form-control" id="basic-icon-default-fullname" placeholder="Gabriela " aria-label="John Doe" aria-describedby="basic-icon-default-fullname2">
+                            </div>
+                          </div>
+                        </div>
+                        <div class="row mb-3">
+                          <label class="col-sm-2 col-form-label" for="basic-icon-default-company">Titulo</label>
+                          <div class="col-sm-10">
+                            <div class="input-group input-group-merge">
+                              <span id="basic-icon-default-company2" class="input-group-text"><i class='bx bx-money'></i></i></span>
+                              <input type="text" id="basic-icon-default-company" class="form-control" placeholder="PIX/ Dinheiro / Cartão" aria-label="ACME Inc." aria-describedby="basic-icon-default-company2">
+                            </div>
+                          </div>
+                        </div>
+                        <div class="row mb-3">
+                          <label class="col-sm-2 col-form-label" for="basic-icon-default-email">Emissão</label>
+                          <div class="col-sm-10">
+                            <div class="input-group input-group-merge">
+                              <span class="input-group-text"><i class='bx bxs-calendar-heart'></i></span>
+                              <input type="text" id="basic-icon-default-email" class="form-control" placeholder="10/01/2020" aria-label="john.doe" aria-describedby="basic-icon-default-email2">
+                              <span id="basic-icon-default-email2" class="input-group-text">10/01/2020</span>
+                            </div>
+                            
 
-            <!-- / Navbar -->
+                            
+                            <div class="form-text">Dia que foi feito o procedimento</div>
+                          </div>
+                        </div>
+                        <div class="row mb-3">
+                          <label class="col-sm-2 form-label" for="basic-icon-default-phone">Valor total</label>
+                          <div class="col-sm-10">
+                            <div class="input-group input-group-merge">
+                              <span id="basic-icon-default-phone2" class="input-group-text"><i class='bx bx-money'></i></span>
+                              <input type="number"  id="basic-icon-default-phone" class="form-control phone-mask" placeholder="400,00" aria-label="658 799 8941" aria-describedby="basic-icon-default-phone2">
+                            </div>
+                          </div>
 
-            <!-- Content wrapper -->
-            <div class="content-wrapper">
-                <!-- Content -->
-                <div class='container-xxl flex-grow-1 container-p-y'>
-                   <div class="row">
-                       <div class="card mb-4">
-                           <div class="card-header d-flex justify-content-between align-items-center">
-                               <h5 class="mb-0">Cadastrar nova conta</h5>
-                               <small class="text-muted float-end">Default label</small>
-                           </div>
-                           <div class="card-body">
-                               <form method="post">
-                                   <div class="mb-3">
-                                       <label class="form-label" for="basic-default-fullname">nome</label>
-                                       <input name="nome" type="text" class="form-control" id="basic-default-fullname">
-                                   </div>
+                          </div>
+                        <div class="row mb-3">
+                          <label class="col-sm-2 form-label" for="basic-icon-default-phone">Valor falta</label>
+                          <div class="col-sm-10">
+                            <div class="input-group input-group-merge">
+                              <span id="basic-icon-default-phone2" class="input-group-text"><i class='bx bx-money'></i></span>
+                              <input type="number"  id="basic-icon-default-phone" class="form-control phone-mask" placeholder="100,00" aria-label="658 799 8941" aria-describedby="basic-icon-default-phone2">
+                            </div>
+                          </div>
 
-                                   <div class="mb-3">
-                                       <label class="form-label" for="basic-default-phone">descricao</label>
-                                       <input name="descrição" type="text" id="basic-default-phone" class="form-control phone-mask" placeholder="coca 2 lts">
-                                   </div>
-
-                                   <div class="mb-3">
-                                       <label class="form-label" for="basic-default-company">quantidade</label>
-                                       <input name="quantidade" type="number" class="form-control" id="basic-default-company">
-                                   </div>
-
-
-                                   <div class="mb-3">
-                                       <label class="form-label" for="basic-default-message">valor</label>
-                                       <input name="valor" type="number" class="form-control" id="basic-default-company">
-
-                                   </div>
-
-                                   <div class="mb-3">
-                                       <label for="exampleFormControlSelect1" class="form-label">status</label>
-                                       <select name="status" class="form-select" id="exampleFormControlSelect1">
-
-                                           <option value="inativo">inativo</option>
-                                           <option selected value="ativo">ativo</option>  <!--  forçando ficar selecionado -->//
-
-
-                                       </select>
-                                   </div>
-
-                                   <button type="submit" name="btncadastrar" class="btn btn-primary">cadastrar</button>
-                               </form>
+                        </div>
+                        <div class="row mb-3">
+                          <label class="col-sm-2 form-label" for="basic-icon-default-message">observação</label>
+                          <div class="col-sm-10">
+                            <div class="input-group input-group-merge">
+                              <span id="basic-icon-default-message2" class="input-group-text"><i class="bx bx-comment"></i></span>
+                              <textarea id="basic-icon-default-message" class="form-control" placeholder="cliente vai pagar o restante na próxima sessão" aria-label="Hi, Do you have a moment to talk Joe?" aria-describedby="basic-icon-default-message2"></textarea>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="row justify-content-end">
+                          <div class="col-sm-10">
+                            <button type="submit" class="btn btn-primary">SALVAR</button>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
                                <?php
                                if(isset($_POST['btncadastrar'])) {
                                    $name = $_POST['nome'];
