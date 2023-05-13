21659530432dc7b74efe50ca94da3821a8240eab
diegosilvaas <diegomecanica2014@gmail.com>
2023-05-13 18:29:00 -0400

    alterando avatar

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ require_once 'Config.php';
               <li class="nav-item navbar-dropdown dropdown-user dropdown">
                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                   <div class="avatar avatar-online">
-                    <img src="assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                    <img src="assets/img/avatars/gabi.png" alt class="w-px-40 h-auto rounded-circle" />
                   </div>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end">
@@ -56,7 +56,7 @@ require_once 'Config.php';
                       <div class="d-flex">
                         <div class="flex-shrink-0 me-3">
                           <div class="avatar avatar-online">
-                            <img src="assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                            <img src="assets/img/avatars/gabi.png" alt class="w-px-40 h-auto rounded-circle" />
                           </div>
                         </div>
                         <div class="flex-grow-1">
