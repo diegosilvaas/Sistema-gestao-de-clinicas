6031439d4f56c0f316bd62ecd97e22a3573f79ee
diegosilvaas <diegomecanica2014@gmail.com>
2023-05-14 22:00:37 -0400

    alterando placeholder da barra de pesquisar

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ require_once 'Config.php';
             <div class="navbar-nav align-items-center">
               <div class="nav-item d-flex align-items-center">
                 <i class="bx bx-search fs-4 lh-0"></i>
-                <input type="text" class="form-control border-0 shadow-none" placeholder="Search..." aria-label="Search..." />
+                <input type="text" class="form-control border-0 shadow-none" placeholder="Pesquisar..." aria-label="Search..." />
               </div>
             </div>
             <!-- /Search -->
